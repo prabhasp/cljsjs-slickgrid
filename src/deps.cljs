b8c51703054d721ff9149b3b155e7d0fbae0a758
William Mucheru <willyk99@gmail.com>
2015-10-28 15:06:52 +0300

    WM: Forked from prbhasp, added autocolumn resize plugin

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
 {
- :foreign-libs [{:file     "slick.core-grid-dataview-pager-jquery-jqdrag.min.js"
-                 :file-min "slick.core-grid-dataview-pager-jquery-jqdrag.min.js"
+ :foreign-libs [{:file     "slick.core-grid-dataview-pager-jquery-jqdrag-autocolumnsize.min.js"
+                 :file-min "slick.core-grid-dataview-pager-jquery-jqdrag-autocolumnsize.min.js"
                  :provides ["cljsjs.slickgrid-with-deps"]}
                 {:file     "slick.core-grid-dataview-pager.min.js"
                  :file-min "slick.core-grid-dataview-pager.min.js"
