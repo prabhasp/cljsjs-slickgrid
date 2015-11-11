6a918f9bd69ad61efd6ced3332170c63b681e793
William Mucheru <willyk99@gmail.com>
2015-11-11 17:12:23 +0300

    WM: Set timeout to ensure autosize attaches to existing handler

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/slickgrid-cljs "0.0.2"
+(defproject onaio/slickgrid-cljs "0.0.3-SNAPSHOT"
  :description "SlickGrid"
  :url "https://github.com/mleibman/SlickGrid"
  :license {:name "Eclipse Public License"
