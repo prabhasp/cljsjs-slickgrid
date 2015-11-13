7acfefddaf5e6820c049c756883ec29ce1d107a5
Peter Lubell-Doughtie <peetldee@gmail.com>
2015-11-13 15:06:05 -0500

    PLD: bump to 002

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/slickgrid-cljs "0.0.2-arbitrary-page-counts-SNAPSHOT"
+(defproject onaio/slickgrid-cljs "0.0.2"
  :description "SlickGrid"
  :url "https://github.com/mleibman/SlickGrid"
  :license {:name "Eclipse Public License"
